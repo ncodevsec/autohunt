f43453c34946c07dff31effc86f6cc4a34de994a
MD NAZMUL HAQUE <nazmul30801@gmail.com>
2025-05-01 13:37:15 +0600

    Add initial .gitignore and subdenum.sh script for subdomain enumeration
    tools added -
      - subfinder
      - sublist3r
      - assetfinder

---
+++ subdenum.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Get Target
+
+
+# Funtions
+silent() {
+    "$@" > /dev/null 2>&1
+}
+
+
+
+# Set Target
+TARGET=$1
+
+# Directory
+DIR="~/data/$TARGET/recon/subdomain"
+
+# Tools to run
+
+# subfinder
+TOOL="subfinder"
+echo -e "\n[+] Running subfinder ..."
+subfinder -d $TARGET -silent -o $DIR/$TOOL.txt
+echo -e "[-] Completed subfinder.\n"
+
+# sublist3r
+TOOL="sublist3r"
+echo -e "\n[+] Running $TOOL ..."
+silent sublist3r -d $TARGET -o $DIR/$TOOL.txt
+echo -e "[-] Completed $TOOL.\n"
+
+# assetfinder
+TOOL="assetfinder"
+echo -e "\n[+] Running $TOOL ..."
+silent assetfinder -subs-only $TARGET > $DIR/$TOOL.txt
+echo -e "[-] Completed $TOOL.\n"
+
+# amass
+
+
+# findomain
+
+
+# ffuf
+
+
+# pureDNS
+
+
+# crt.sh
+
+
+echo "\n[-]Scan Complete.\n\nSubdomains are saved in - $DIR/$TOOL.txt"
