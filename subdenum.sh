d2112258583f52fe9b4f27581d8b4e01fde8de01
MD NAZMUL HAQUE <nazmul30801@gmail.com>
2025-05-01 16:49:52 +0600

    Refactor code structure for improved readability and maintainability

--- subdenum.sh
+++ subdenum.sh
@@ -1,57 +1,70 @@
 #!/bin/bash
 
-# Get Target
+# Demo Command
+# subdenum yahoo.com wordlist.txt resolver.txt output_dir
 
+# Input Variables
+# TARGET=$1
+# WORDLIST=$2
+# RESOLVER=$3
+# OUTPUT_DIR=$4
+
+# Input Variables
+TARGET=$1
+WORDLIST="/usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt"
+RESOLVER="resolver.txt"
+OUTPUT_DIR="$HOME/data/$TARGET/recon/subdomain"
+if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir -p "$OUTPUT_DIR"
+fi
 
 # Funtions
-silent() {
-    "$@" > /dev/null 2>&1
-}
-
-
-
-# Set Target
-TARGET=$1
-
-# Directory
-DIR="~/data/$TARGET/recon/subdomain"
-if [ ! -d "$DIR" ]; then
-    mkdir -p "$DIR"
-fi
+# silent_out() {
+#     "$@" > $OUTPUT_DIR/$TOOL.txt > /dev/null 2>&1
+# }
 
 # Tools to run
 
 # subfinder
 TOOL="subfinder"
 echo -e "\n[+] Running subfinder ..."
-subfinder -d $TARGET -silent -o $DIR/$TOOL.txt
+subfinder -d $TARGET > $OUTPUT_DIR/$TOOL.txt > /dev/null 2>&1
 echo -e "[-] Completed subfinder.\n"
 
 # sublist3r
 TOOL="sublist3r"
 echo -e "\n[+] Running $TOOL ..."
-silent sublist3r -d $TARGET -o $DIR/$TOOL.txt
+sublist3r -d $TARGET > $OUTPUT_DIR/$TOOL.txt > /dev/null 2>&1
 echo -e "[-] Completed $TOOL.\n"
 
 # assetfinder
 TOOL="assetfinder"
 echo -e "\n[+] Running $TOOL ..."
-silent assetfinder -subs-only $TARGET > $DIR/$TOOL.txt
+assetfinder -subs-only $TARGET > $OUTPUT_DIR/$TOOL.txt > /dev/null 2>&1
 echo -e "[-] Completed $TOOL.\n"
 
 # amass
 
 
 # findomain
-
+TOOL="findomail"
+echo -e "\n[+] Running $TOOL ..."
+findomain -q -t $TARGET > $OUTPUT_DIR/$TOOL.txt > /dev/null 2>&1
+echo -e "[-] Completed $TOOL.\n"
 
 # ffuf
-
+TOOL="findomail"
+echo -e "\n[+] Running $TOOL ..."
+findomain -q -t $TARGET > $OUTPUT_DIR/$TOOL.txt > /dev/null 2>&1
+echo -e "[-] Completed $TOOL.\n"
 
 # pureDNS
-
+TOOL="findomail"
+echo -e "\n[+] Running $TOOL ..."
+puredns bruteforce /usr/share/ bing.com -q -r resolver.txt > $OUTPUT_DIR/$TOOL.txt > /dev/null 2>&1
+echo -e "[-] Completed $TOOL.\n"
 
 # crt.sh
 
 
-echo "\n[-]Scan Complete.\n\nSubdomains are saved in - $DIR/$TOOL.txt"
+echo -e "\n[-]Scan Complete.\n\nSubdomains are saved in - $OUTPUT_DIR/$TOOL.txt"
