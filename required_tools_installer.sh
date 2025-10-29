d967c7d90e6aab4c2c884cb07397b9c695a54d4e
nCodevSec <117487836+ncodevsec@users.noreply.github.com>
2025-10-29 07:23:34 +0600

    Refactor required_tools_installer.sh for clarity
    Reordering tools list for perfect install

--- required_tools_installer.sh
+++ required_tools_installer.sh
@@ -8,10 +8,29 @@ grep -qxF 'export PATH=$PATH:/usr/local/go/bin' ~/.bashrc || echo 'export PATH=$
 grep -qxF 'export GOPATH=$HOME/go' ~/.bashrc || echo 'export GOPATH=$HOME/go' >> ~/.bashrc
 grep -qxF 'export PATH=$PATH:$GOPATH/bin' ~/.bashrc || echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
 source ~/.bashrc
-go version
 
 # Assetfinder
 go install github.com/tomnomnom/assetfinder@latest
+
+# Subfinder
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
+# Amass
+go install github.com/owasp-amass/amass/v4/cmd/amass@latest
+
+# HTTPX
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+# Gowitness
+go install github.com/sensepost/gowitness@latest
+
+# Puredns
+git clone https://github.com/blechschmidt/massdns.git
+cd massdns
+make
+sudo make install
+go install github.com/d3mondev/puredns/v2@latest
+cd ../
 
 # Findomain
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -20,30 +39,13 @@ git clone https://github.com/findomain/findomain.git
 cd findomain
 cargo build --release
 sudo cp target/release/findomain /usr/bin/
-
-# Puredns
-git clone https://github.com/blechschmidt/massdns.git
-cd massdns
-make
-sudo make install
-go install github.com/d3mondev/puredns/v2@latest
-
-# Subfinder
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+cd ../
 
 # Sublist3r
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r
 sudo pipx install sublist3r
-
-# Amass
-go install github.com/owasp-amass/amass/v4/cmd/amass@latest
+cd ../
 
 # CSVkit
 sudo pipx install csvkit
-
-# HTTPX
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-
-# Gowitness
-go install github.com/sensepost/gowitness@latest
